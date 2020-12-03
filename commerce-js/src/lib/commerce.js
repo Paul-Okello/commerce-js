f316bba241871d1785a8b7a6d7f0593002358453
Paul Okello <paulokello273@gmail.com>
2020-12-03 18:09:07 +0300

    commercejs

--- commerce-js/src/lib/commerce.js
+++ commerce-js/src/lib/commerce.js
@@ -1,3 +1,6 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(process.env.REACT_APP_COMMERCE_JS_KEY);
+export const commerce = new Commerce(
+  process.env.REACT_APP_COMMERCE_JS_KEY,
+  true
+);
