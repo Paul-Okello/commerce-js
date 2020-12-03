ea2ea59d944b29bbece129e9d75e05ecae40371f
Paul Okello <paulokello273@gmail.com>
2020-12-03 10:41:12 +0300

    product styling

--- commerce-js/src/components/Products/Product/styles.js
+++ commerce-js/src/components/Products/Product/styles.js
@@ -4,4 +4,16 @@ export default makeStyles(() => ({
   root: {
     maxWidth: "100%",
   },
+  media: {
+    margin: 0,
+    paddingTop: "56.25%",
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space between",
+  },
 }));
