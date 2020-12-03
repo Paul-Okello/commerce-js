8507db3e4edeeb494a9b7319704ccf2c9f4a8339
Paul Okello <paulokello273@gmail.com>
2020-12-03 15:42:00 +0300

    importing modules

--- commerce-js/src/App.js
+++ commerce-js/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import Products from "./components/Products/Products";
+import { Products, Navbar } from "./components";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
