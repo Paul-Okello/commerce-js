f23d86e556373e1ee7de432ad2b5f69ba084c3bb
Paul Okello <paulokello273@gmail.com>
2020-12-03 18:09:18 +0300

    commeercejs setup

--- commerce-js/src/App.js
+++ commerce-js/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./components";
 
 function App() {
