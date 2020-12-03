54a61727d3f50321544f230278f850247ae68957
Paul Okello <paulokello273@gmail.com>
2020-12-03 20:23:27 +0300

    filledcart

---
+++ commerce-js/src/components/Cart/Cart.js
@@ -0,0 +1,56 @@
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+import React from "react";
+
+const Cart = ({ cart }) => {
+  const isEmpty = !cart.line_items.length;
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">No Items In Your Shopping Cart</Typography>
+  );
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+          >
+            Empty Cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3">
+        Your Shopping Cart
+      </Typography>
+      {isEmpty ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
