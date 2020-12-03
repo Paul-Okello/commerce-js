3123f070674a8ab074cb952b9c400478fd08119f
Paul Okello <paulokello273@gmail.com>
2020-12-03 18:08:58 +0300

    fetch products from commercejs

--- commerce-js/src/App.js
+++ commerce-js/src/App.js
@@ -2,6 +2,16 @@ import React from "react";
 import { Products, Navbar } from "./components";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
   return (
     <div>
       <Navbar />
