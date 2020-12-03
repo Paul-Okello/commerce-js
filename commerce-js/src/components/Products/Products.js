2ebc74dce1df4d406b6c40625b5474c912819674
Paul Okello <paulokello273@gmail.com>
2020-12-03 10:26:56 +0300

    more styling

--- commerce-js/src/components/Products/Products.js
+++ commerce-js/src/components/Products/Products.js
@@ -2,9 +2,9 @@ import React from "react";
 import Grid from "@material-ui/core";
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running Shoes" },
-  { id: 2, name: "PS 5", description: "Gamming" },
-  { id: 3, name: "Trouser", description: "Lower body wear" },
+  { id: 1, name: "Shoes", description: "Running Shoes" , price="Ksh 3000" },
+  { id: 2, name: "PS 5", description: "Gamming", price="Ksh 53000" },
+  { id: 3, name: "Trouser", description: "Lower body wear", price="Ksh 1500" },
 ];
 
 const Products = () => {
@@ -13,7 +13,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={2}>
-            <Product />
+            <Product product={product} />
           </Grid>
         ))}
       </Grid>
