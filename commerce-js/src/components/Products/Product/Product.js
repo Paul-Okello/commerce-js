c50857b03e430603cf4cc8051da4c0e210b2dc41
Paul Okello <paulokello273@gmail.com>
2020-12-03 10:26:46 +0300

    debugging

--- commerce-js/src/components/Products/Product/Product.js
+++ commerce-js/src/components/Products/Product/Product.js
@@ -1,13 +1,27 @@
-import { Card, CardMedia } from "@material-ui/core";
+import { Card, CardContent, CardMedia, IconButton, Typography } from "@material-ui/core";
 import React from "react";
+import {AddShoppingCart} from "@material-ui/icons"
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
-    <div>
-      <Card>
-        <CardMedia className={classes.media} image="" title={product.name} />
-      </Card>
-    </div>
+    <Card>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+    </CardActions>
   );
 };
 
