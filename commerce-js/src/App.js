fb20a57b824c972b5cc0c7c45a4bc32b99f26ddd
Paul Okello <paulokello273@gmail.com>
2020-12-06 16:31:50 +0300

    React Router Dom setup 😍

--- commerce-js/src/App.js
+++ commerce-js/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { commerce } from "./lib/commerce";
 import { Products, Navbar, Cart } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -22,14 +23,21 @@ function App() {
     fetchProducts();
     fetchCart();
   }, []);
-  console.log(products);
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
-      <Cart cart={cart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
