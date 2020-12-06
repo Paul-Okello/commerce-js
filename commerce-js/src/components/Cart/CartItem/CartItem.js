5e7141184c8c2818af86c161be91b21f2e0885a4
Paul Okello <paulokello273@gmail.com>
2020-12-06 16:07:56 +0300

    CartItem Setup 😊

--- commerce-js/src/components/Cart/CartItem/CartItem.js
+++ commerce-js/src/components/Cart/CartItem/CartItem.js
@@ -1,8 +1,43 @@
+import {
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import useStyles from "./styles";
 
-const CartItem = () => {
-  return <div></div>;
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardMedia
+        image={item.media.source}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h6">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_special}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default CartItem;
