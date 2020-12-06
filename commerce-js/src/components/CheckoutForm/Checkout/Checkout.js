919593a603b977a3a52c24b00b1a8d04f5923b8a
Paul Okello <paulokello273@gmail.com>
2020-12-06 17:31:52 +0300

    Checkout setup  😃

---
+++ commerce-js/src/components/CheckoutForm/Checkout/Checkout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+
+const steps = ["Shipping Address", "Payment Details"];
+
+const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper classname={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={0} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
