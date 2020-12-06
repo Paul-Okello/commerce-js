bccf4f0755a5386962debf89d9e63e8ee4dbd9f2
Paul Okello <paulokello273@gmail.com>
2020-12-06 18:17:59 +0300

    form input

--- commerce-js/src/components/CheckoutForm/AddressFrom.js
+++ commerce-js/src/components/CheckoutForm/AddressFrom.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const AddressFrom = () => {
-  return <div>Address Form</div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h5" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address" label="Address" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="zip" label="ZIP / Postal code" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressFrom;
