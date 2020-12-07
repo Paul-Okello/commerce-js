8bb806646c43b6fb2a9ccc6b13d16a1a64b13506
Paul Okello <paulokello273@gmail.com>
2020-12-07 13:31:39 +0300

    stripe setup ✍

--- commerce-js/src/components/CheckoutForm/PaymentFrom.js
+++ commerce-js/src/components/CheckoutForm/PaymentFrom.js
@@ -1,4 +1,12 @@
 import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import {
+  Elements,
+  CardElement,
+  ElementConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Review from "./Review";
 
 const PaymentFrom = () => {
   return <div>Payment Form</div>;
