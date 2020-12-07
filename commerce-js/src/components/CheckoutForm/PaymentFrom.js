661c6e5d1ad5940284781e0c41a6e52330fb8e4f
Paul Okello <paulokello273@gmail.com>
2020-12-07 14:39:10 +0300

    stripe setup

--- commerce-js/src/components/CheckoutForm/PaymentFrom.js
+++ commerce-js/src/components/CheckoutForm/PaymentFrom.js
@@ -8,8 +8,60 @@ import {
 import { loadStripe } from "@stripe/stripe-js";
 import Review from "./Review";
 
-const PaymentFrom = () => {
-  return <div>Payment Form</div>;
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+
+const PaymentFrom = ({ checkoutToken, backStep }) => {
+  const handleSubmit = (e, elements, stripe) => {
+    e.preventDefault();
+    if (!stripe || !elements) return;
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log(error);
+    } else {
+      const orderData = {
+        line_items: checkoutToken.live.line_items,
+      };
+    }
+  };
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant="h6" gutterBottom style={{ margin: "20px 0" }}>
+        Payment method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementConsumer>
+          {({ elements, stripe }) => (
+            <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
+              <CardElement />
+              <br /> <br />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Button variant="outlined" onClick={backStep}>
+                  Back
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={!stripe}
+                  color="primary"
+                >
+                  Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                </Button>
+              </div>
+            </form>
+          )}
+        </ElementConsumer>
+      </Elements>
+    </>
+  );
 };
 
 export default PaymentFrom;
